ad162fc41b9f9af11a53f84c2eeb00ecf4cb7e09
majudev <mariajuliadev@gmail.com>
2025-03-12 11:26:53 -0300

    feat: Adicionando implementações funcionais das areas de transações e saldo da conta

--- src/components/componentsFinances/accountBalance/index.tsx
+++ src/components/componentsFinances/accountBalance/index.tsx
@@ -1,59 +1,92 @@
+import { collection, onSnapshot } from 'firebase/firestore';
 import styles from '../../../styles/accountBalance.module.css';
+import { useState, useEffect } from 'react';
+import { db } from '../../../services/firebaseConnection';
+
+interface TransactionsProps {
+    value: number;
+    type: 'exit' | 'entry';
+    date: string;
+    description: string;
+}
 
 export function AccountBalance() {
+    const [transactions, setTransactions] = useState<TransactionsProps[]>()
+
+    useEffect(() => {
+
+        async function getTransactions() {
+
+            onSnapshot(collection(db, 'finances-transactions'), (snapshot) => {
+                let list: TransactionsProps[] = [];
+
+                snapshot.forEach((doc) => {
+                    list.push({
+                        value: doc.data().value,
+                        type: doc.data().type,
+                        date: doc.data().date,
+                        description: doc.data().description
+                    });
+                });
+
+                setTransactions(list);
+            })
+
+        }
+        getTransactions()
+
+    }, [])
+
+    const total = transactions?.reduce((acc, item) => acc + item.value, 0)
+
     return (
         <div className={styles.accountBalance}>
 
             <div className={styles.headerPayment}>
                 <h1 className={styles.title}>Saldo da conta</h1>
-                <span>Total: R$ 450</span>
+                <span>Total: {total?.toLocaleString('pt-BR', {
+                    style: 'currency',
+                    currency: 'BRL'
+                })}</span>
             </div>
 
             <div className={styles.tableAreaAccountBalance}>
-                <table className={styles.tableAccountBalance}>
 
-                    <thead>
-                        <tr>
-                            <th>Valor</th>
-                            <th>Descrição</th>
-                            <th>Data</th>
-                        </tr>
-                    </thead>
 
-                    <tbody>
-                        <tr>
-                            <td>R$ 450</td>
-                            <td>Mamãe sonia me deu</td>
-                            <td>12/03/2025</td>
-                        </tr>
-                        <tr>
-                            <td>R$ - 450</td>
-                            <td>Mamãe sonia me deu </td>
-                            <td>12/03/2025</td>
-                        </tr>
-                        <tr>
-                            <td>R$ 450</td>
-                            <td>Mamãe sonia me deu</td>
-                            <td>12/03/2025</td>
-                        </tr>
-                        <tr>
-                            <td>R$ 450</td>
-                            <td>Mamãe sonia me deu</td>
-                            <td>12/03/2025</td>
-                        </tr>
-                        <tr>
-                            <td>R$ 450</td>
-                            <td>Mamãe sonia me deu</td>
-                            <td>12/03/2025</td>
-                        </tr>
-                        <tr>
-                            <td>R$ 450</td>
-                            <td>Mamãe sonia me deu</td>
-                            <td>12/03/2025</td>
-                        </tr>
-                    </tbody>
+                {transactions?.length === 0 ? (
+                    <div>
+                        <h1>teste</h1>
+                    </div>
+                ) :
+                    <table className={styles.tableAccountBalance}>
+                        <thead>
+                            <tr>
+                                <th>Valor</th>
+                                <th>Descrição</th>
+                                <th>Data</th>
+                            </tr>
+                        </thead>
 
-                </table>
+                        <tbody>
+                            {transactions?.map((item, index) => (
+                                <tr key={index}>
+                                    <td>
+                                        <span className={item.type === 'entry' ? `${styles.valueEntry}` : `${styles.valueExit}`}>
+                                            {(item.value).toLocaleString('pt-BR', {
+                                                style: 'currency',
+                                                currency: 'BRL'
+                                            })}
+                                        </span>
+                                    </td>
+                                    <td>{item.description}</td>
+                                    <td>{item.date}</td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
+                }
+
+
             </div>
         </div >
     )
--- src/components/componentsFinances/debitMoviment/index.tsx
+++ src/components/componentsFinances/debitMoviment/index.tsx
@@ -1,51 +1,126 @@
-import styles from '../../../styles/debitMoviment.module.css';
+import styles from '../../../styles/transactions.module.css';
 import { FaArrowAltCircleDown, FaArrowAltCircleUp } from "react-icons/fa";
+import { useState } from 'react';
+import { auth, db } from '../../../services/firebaseConnection';
+import { addDoc, collection } from 'firebase/firestore';
+import { toast } from 'react-toastify';
+import { formatarData } from '../../../utils/dataFormatada';
+
+interface TransactionsProps {
+    entryDescription: string;
+    entryValue: string;
+    exitDescription: string;
+    exitValue: string
+}
 
 export function DebitMoviment() {
+    const [inputValue, setInputValue] = useState<TransactionsProps>({
+        entryDescription: '',
+        entryValue: '',
+        exitDescription: '',
+        exitValue: ''
+    })
+
+
+    async function handleAddEntry() {
+        if (!inputValue.entryValue || !inputValue.entryDescription) {
+            return toast.warning('Preencha todos os campos!')
+        }
+
+        const date = formatarData(String(new Date())).dataFormatada
+
+        await addDoc(collection(db, 'finances-transactions'), {
+            value: Number(inputValue.entryValue),
+            description: inputValue.entryDescription,
+            type: 'entry',
+            date: date,
+            userId: auth.currentUser?.uid
+        })
+
+        setInputValue({
+            entryDescription: '',
+            entryValue: '',
+            exitDescription: '',
+            exitValue: ''
+        })
+    }
+
+    async function handleAddExit() {
+        if (!inputValue.exitValue || !inputValue.exitDescription) {
+            return toast.warning('Preencha todos os campos!')
+        }
+
+        const date = formatarData(String(new Date())).dataFormatada
+
+        await addDoc(collection(db, 'finances-transactions'), {
+            value: Number(-inputValue.exitValue),
+            description: inputValue.exitDescription,
+            type: 'exit',
+            date: date,
+            userId: auth.currentUser?.uid
+        })
+
+        setInputValue({
+            entryDescription: '',
+            entryValue: '',
+            exitDescription: '',
+            exitValue: ''
+        })
+    }
+
+
     return (
-        <div className={styles.debitMoviment}>
+        <div className={styles.transactions}>
 
-            <h1 className={styles.title}>Movimentação no débito</h1>
+            <h1 className={styles.title}>Transações</h1>
 
-            <div className={styles.areaMoviment}>
+            <div className={styles.areaTransactions}>
                 <div className={styles.exits}>
 
-                    <div className={styles.inputsAreaDebitMoviment}>
+                    <div className={styles.inputsAreaTransactions}>
                         <span>Adicionar saídas</span>
                         <input
                             type='number'
-                            placeholder='R$ 450'
-                            className={styles.inputDebitMoviment}
+                            placeholder='R$ -450'
+                            className={styles.inputTransactions}
+                            value={inputValue.exitValue}
+                            onChange={(e) => setInputValue(prev => ({ ...prev, exitValue: e.target.value }))}
                         />
                         <input
                             type='text'
                             placeholder='Descrição'
-                            className={styles.inputDebitMoviment}
+                            className={styles.inputTransactions}
+                            value={inputValue.exitDescription}
+                            onChange={(e) => setInputValue(prev => ({ ...prev, exitDescription: e.target.value }))}
                         />
                     </div>
 
-                    <button className={styles.btnDebitMoviment}>
+                    <button className={styles.btnTransactions} onClick={handleAddExit}>
                         <FaArrowAltCircleDown size={40} color='#fff' />
                     </button>
                 </div>
 
                 <div className={styles.entries}>
 
-                    <div className={styles.inputsAreaDebitMoviment}>
+                    <div className={styles.inputsAreaTransactions}>
                         <span>Adicionar entrada</span>
                         <input
                             type='number'
                             placeholder='R$ 450'
-                            className={styles.inputDebitMoviment}
+                            className={styles.inputTransactions}
+                            value={inputValue.entryValue}
+                            onChange={(e) => setInputValue(prev => ({ ...prev, entryValue: e.target.value }))}
                         />
                         <input
                             type='text'
                             placeholder='Descrição'
-                            className={styles.inputDebitMoviment}
+                            className={styles.inputTransactions}
+                            value={inputValue.entryDescription}
+                            onChange={(e) => setInputValue(prev => ({ ...prev, entryDescription: e.target.value }))}
                         />
                     </div>
 
-                    <button className={styles.btnDebitMoviment}>
+                    <button className={styles.btnTransactions} onClick={handleAddEntry}>
                         <FaArrowAltCircleUp size={40} color='#fff' />
                     </button>
 
