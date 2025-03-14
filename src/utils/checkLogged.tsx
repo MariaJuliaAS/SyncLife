adc46d8f81ac3cc467fb1b48cfee72461b674386
majudev <mariajuliadev@gmail.com>
2025-03-14 16:51:20 -0300

    ajustando arquivos

--- src/utils/checkLogged.tsx
+++ src/utils/checkLogged.tsx
@@ -1,4 +1,3 @@
-import { useEffect } from "react";
 import { auth } from "../services/firebaseConnection";
 import { useNavigate } from "react-router-dom";
 import { onAuthStateChanged } from "firebase/auth";
@@ -6,16 +5,15 @@ import { onAuthStateChanged } from "firebase/auth";
 const CheckLogged = () => {
     const navigate = useNavigate();
 
-    useEffect(() => {
 
-        onAuthStateChanged(auth, (user) => {
-            if (!user || !user.emailVerified) {
-                navigate('/', { replace: true })
-            }
 
-        })
+    onAuthStateChanged(auth, (user) => {
+        if (!user || !user.emailVerified) {
+            navigate('/', { replace: true })
+        }
 
-    }, [])
+    })
+
 
 }
 
