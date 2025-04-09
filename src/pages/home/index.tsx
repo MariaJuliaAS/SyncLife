de006fe9e86ad0003a0db47d709669734412ffa6
MariaJulia <mariajuliadev@gmail.com>
2025-04-09 16:47:12 -0300

    Implementação estatica do calendario

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,7 +1,32 @@
+import FullCalendar from '@fullcalendar/react';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import timeGridPlugin from '@fullcalendar/timegrid';
+import listPlugin from '@fullcalendar/list';
+import interactionPlugin, { DateClickArg, EventResizeDoneArg } from '@fullcalendar/interaction';
+import bootstrap5Plugin from '@fullcalendar/bootstrap5';
+import ptLocale from '@fullcalendar/core/locales/pt'
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
+import './calendar.css'
 
 
 export function Home() {
     return (
-        <h1>PÁGINA DE HOME</h1>
+        <FullCalendar
+            plugins={[dayGridPlugin, interactionPlugin, timeGridPlugin, listPlugin, bootstrap5Plugin]}
+            themeSystem="bootstrap5"
+            initialView="dayGridMonth"
+            headerToolbar={{
+                left: 'prev,next today',
+                center: 'title',
+                right: 'dayGridMonth,timeGridWeek,timeGridDay,listWeek'
+            }}
+            locale='pt'
+            locales={[ptLocale]}
+            events={[
+                { title: 'Evento 1', date: '2025-04-09' },
+                { title: 'Evento 2', date: '2025-04-10' },
+            ]}
+        />
     )
 }
