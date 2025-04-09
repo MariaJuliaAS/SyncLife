6c7b864b987c47631f709be235b0ced8ad39facb
MariaJulia <mariajuliadev@gmail.com>
2025-04-09 17:13:23 -0300

    Estilização do calendario

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -3,30 +3,29 @@ import dayGridPlugin from '@fullcalendar/daygrid';
 import timeGridPlugin from '@fullcalendar/timegrid';
 import listPlugin from '@fullcalendar/list';
 import interactionPlugin, { DateClickArg, EventResizeDoneArg } from '@fullcalendar/interaction';
-import bootstrap5Plugin from '@fullcalendar/bootstrap5';
 import ptLocale from '@fullcalendar/core/locales/pt'
-import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap-icons/font/bootstrap-icons.css';
 import './calendar.css'
-
 
 export function Home() {
     return (
-        <FullCalendar
-            plugins={[dayGridPlugin, interactionPlugin, timeGridPlugin, listPlugin, bootstrap5Plugin]}
-            themeSystem="bootstrap5"
-            initialView="dayGridMonth"
-            headerToolbar={{
-                left: 'prev,next today',
-                center: 'title',
-                right: 'dayGridMonth,timeGridWeek,timeGridDay,listWeek'
-            }}
-            locale='pt'
-            locales={[ptLocale]}
-            events={[
-                { title: 'Evento 1', date: '2025-04-09' },
-                { title: 'Evento 2', date: '2025-04-10' },
-            ]}
-        />
+        <main className='sm:px-12 w-full max-h-screen py-6 px-4 flex justify-center '>
+            <FullCalendar
+                plugins={[dayGridPlugin, interactionPlugin, timeGridPlugin, listPlugin]}
+                themeSystem="standard"
+                initialView={window.innerWidth <= 850 ? 'timeGridDay' : 'timeGridWeek'}
+                headerToolbar={{
+                    left: 'prev,next today',
+                    center: 'title',
+                    right: 'dayGridMonth,timeGridWeek,timeGridDay,listWeek'
+                }}
+                locale='pt'
+                locales={[ptLocale]}
+                events={[
+                    { title: 'Evento 1', date: '2025-04-09' },
+                    { title: 'Evento 2', date: '2025-04-10' },
+                ]}
+            />
+        </main>
+
     )
 }
