989c46529c6f37d4a652ff41bc7b14c0b952acbe
MariaJulia <mariajuliadev@gmail.com>
2025-04-11 15:56:26 -0300

    Ajuste na responsividade do header da página de finanças

--- src/pages/finances/index.tsx
+++ src/pages/finances/index.tsx
@@ -10,7 +10,7 @@ export function Finances() {
 
             <div className='sm:px-12 w-full min-h-screen py-6 px-4 flex bg-gray-50 felx justify-center'>
                 <main className="w-full max-w-7xl">
-                    <header className="w-full flex justify-between lg:pt-0 pt-6">
+                    <header className="sm:flex-row flex-col gap-4 w-full flex justify-between lg:pt-0 sm:pt-6 pt-0">
                         <div>
                             <h1 className="font-bold sm:text-3xl text-2xl">Dashboard Financeiro</h1>
                             <span className="sm:text-base text-sm mt-2 text-gray-500">Acompanhe suas finanças em um só lugar</span>
