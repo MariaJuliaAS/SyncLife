6a41a2fac8fd474fed8a94846881693df15c3a28
MariaJulia <mariajuliadev@gmail.com>
2025-04-11 12:56:39 -0300

    Ajuste no arquivo

--- src/components/sidebar/index.tsx
+++ src/components/sidebar/index.tsx
@@ -16,11 +16,11 @@ export function Siderbar() {
             <div>
                 <header className="flex my-5">
                     <GrTarget color="#fff" className="sm:text-4xl text-3xl mr-2 bg-emerald-600 rounded-xl p-1" />
-                    <p className="sm:text-3xl font-bold text-2xl select-none">SyncLife</p>
+                    <p className="sm:text-3xl font-bold text-2xl select-none text-gray-800">SyncLife</p>
                 </header>
 
                 <nav className="flex flex-col gap-4 pt-7 ">
-                    <Link to='/' className="select-none cursor-pointer flex items-center text-lg rounded-md py-1 transition-all duration-200 hover:bg-emerald-600/10 hover:px-4 hover:text-emerald-600">
+                    <Link to='/' className="select-none cursor-pointer flex items-center text-lg  rounded-md py-1 transition-all duration-200 hover:bg-emerald-600/10 hover:px-4 hover:text-emerald-600">
                         <GoCalendar size={25} className="mr-3" />
                         Agenda
                     </Link>
