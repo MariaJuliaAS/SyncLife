ceb30b1745cd7fabb58b951ef8f56fa4184d3794
MariaJulia <mariajuliadev@gmail.com>
2025-04-14 15:47:12 -0300

    Ajuste no header do modal de todas as transações

--- src/components/finances/modal/modalAllTransactions.tsx
+++ src/components/finances/modal/modalAllTransactions.tsx
@@ -22,7 +22,7 @@ export function ModalAllTransactions({ closeModal }: ModalAllTransactionsProps) 
             <main onClick={(e) => e.stopPropagation()} className="max-h-11/12 overflow-y-auto bg-white w-11/12 max-w-xl h-auto flex flex-col rounded-lg p-8 ">
                 <header className="border-b border-gray-200">
                     <div className="flex  justify-between mb-2">
-                        <p className="font-bold sm:text-lg text-base">Nova Transação</p>
+                        <p className="font-bold sm:text-lg text-base">Todas as Transações</p>
                         <MdOutlineClose onClick={closeModal} size={25} className="cursor-pointer mb-4 text-black transition-all duration-200 hover:text-red-500" />
                     </div>
                 </header>
